19c8aabdd35cd91d36d05f9e06b786b68fe0b5b5
msarton5 <55455852+msarton5@users.noreply.github.com>
2019-12-06 13:19:23 -0500

    made some progress

--- js/shapes.js
+++ js/shapes.js
@@ -22,21 +22,27 @@ window.onload = function() {
     document.getElementById("rectangle").onclick = drawRectangle;
 }
 
-/*
- * Exercise 1.
- */
+// exercise 1
 
 const sayHello = function() {
-    
+  var text = canvas.prompt("Message:");
+  while (text.length > 50) {
+    alert("Your message is too long. Keep it under 50 characters.");
+  }
+  var canvas = document.getElementById("student-canvas-1");
+  var ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.font = "48px sans-serif";
+  ctx.clearRect(0 , 0 , canvas.width , canvas.height);
+  ctx.strokeText(30 , 70 , text)
+
 };
 
-/*
- * Exercise 2.
- */
+// exercise 2
 
 const drawRectangle = function() {
 
-    let canvas = document.getElementById("student-canvas-1");
+    let canvas = document.getElementById("student-canvas-2");
     let ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.rect(20, 20, 100, 200);
@@ -46,33 +52,25 @@ const drawRectangle = function() {
     const width = Number(readlineSync.question("Width: "));
 };
 
-/*
- * Exercise 3.
- */
+// exercise 3
 
 const drawColoredRectangle = function() {
     // write your exercise 3 code here
 };
 
-/*
- * Exercise 4.
- */
+// exercise 4
 
 const drawTriangle = function() {
     // write your exercise 4 code here
 };
 
-/*
- * Exercise 5.
- */
+// exercise 5
 
 const drawFace = function() {
     // write your exercise 4 code here
 };
 
-/*
- * Exercise 6 (extra credit).
- */
+// exercise 6
 
 const drawPyramid = function() {
     // write your exercise 5 code here
